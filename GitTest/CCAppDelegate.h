20ab67b92fad2a1ac3349a89585af759b1ae79de
Warren Deshazo <warrendeshazo@Warrens-MacBook-Pro.local>
2013-10-23 01:07:09 -0400

    Initial Commit

---
+++ GitTest/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  GitTest
+//
+//  Created by Warren Deshazo on 10/23/13.
+//  Copyright (c) 2013 Warren Deshazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  GitTest
+//
+//  Created by Warren Deshazo on 10/23/13.
+//  Copyright (c) 2013 Warren Deshazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
