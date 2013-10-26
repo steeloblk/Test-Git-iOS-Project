f9bb98fad00286e175c9952fe24ae4fcb859488e
Warren Deshazo <warrendeshazo@Warrens-MacBook-Pro.local>
2013-10-26 03:15:30 -0400

    added a test2

---
+++ GitTest/CCThirdsACharn.h
@@ -0,0 +1,13 @@
+//
+//  CCThirdsACharn.h
+//  GitTest
+//
+//  Created by Warren Deshazo on 10/25/13.
+//  Copyright (c) 2013 Warren Deshazo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCThirdsACharn : NSObject
+
+@end
---
+++ GitTest/CCThirdsACharn.m
@@ -0,0 +1,13 @@
+//
+//  CCThirdsACharn.m
+//  GitTest
+//
+//  Created by Warren Deshazo on 10/25/13.
+//  Copyright (c) 2013 Warren Deshazo. All rights reserved.
+//
+
+#import "CCThirdsACharn.h"
+
+@implementation CCThirdsACharn
+
+@end
